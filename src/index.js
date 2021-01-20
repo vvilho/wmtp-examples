a5df26d59d7ec6ed108925ac9b40c94caf5da2e8
Vilho Voutilainen <vilho.voutilainen@gmail.com>
2021-01-20 11:45:36 +0200

    ei toimi...

--- src/index.js
+++ src/index.js
@@ -1,88 +1,119 @@
-import Lunchmenu from './assets/fazer.json';
+// Set the min and max numbers for the game as well as the max count number
 
-console.log('Week2 teht 2');
+const minNumber = 0;
+const maxNumber = 100;
 
-//A
-const list = [
-  {name: 'Lingonberry jam', price: 4.00},
-  {name: 'Mushroom and bean casserole', price: 5.50},
-  {name: 'Chili-flavoured wheat', price: 3.00},
-  {name: 'Vegetarian soup', price: 4.80},
-  {name: 'Pureed root vegetable soup with smoked cheese', price: 8.00}
-];
 
-const regex = /^[A-ZÖÄÅ]{1}[A-ZÖÄÅa-zöäå0-9-/,;() ]{4,64}$/;
 
-const validateMeal = (menu) => {
-  for (const i of menu) {
-    console.log(regex.test(i.name), i.name);
+let randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+let guessed = 'no';
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const guessInputs = [];
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+gameGuide.innerHTML = `I have selected a random number between ${minNumber}
+and ${maxNumber}.
+I'll tell you if your guess was too high or too low.`;
+
+
+const checkGuess = () => {
+
+  let userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = 'Previous guesses: ';
+
+  }
+  guesses.textContent += userGuess + ' ';
+  guessInputs.push(userGuess);
+
+  if (userGuess === randomNumber) {
+
+
+    if (guessCount == 1){
+      lastResult.textContent = `Congratulations! You got it right! It took you ${guessCount} guess`;
+    }else{
+      lastResult.textContent = `Congratulations! You got it right! It took you ${guessCount} guesses`;
+
+    }
+    guessed = 'yes';
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    setGameOver();
+
+  } else {
+    lastResult.textContent = 'Wrong!';
+    lastResult.style.backgroundColor = 'red';
+    if(userGuess < randomNumber && userGuess >= minNumber) {
+      lowOrHi.textContent = 'Last guess was too low!';
+    } else if(userGuess > randomNumber && userGuess <= maxNumber) {
+      lowOrHi.textContent = 'Last guess was too high!';
+    } else if (userGuess < minNumber) {
+      lowOrHi.textContent = 'Last guess was out of range!';
+    }else if (userGuess > maxNumber) {
+      lowOrHi.textContent = 'Last guess was out of range!';
+    }
+  }
+
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
+};
+
+guessSubmit.addEventListener('click', checkGuess);
+
+const setGameOver = () => {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Start new game';
+  document.body.append(resetButton);
+  resetButton.addEventListener('click', resetGame);
+};
+
+const resetGame = () => {
+  guessCount = 1;
+  guessed = 'no';
+
+  const resetParas = document.querySelectorAll('.resultParas p');
+  for (let i = 0 ; i < resetParas.length ; i++) {
+    resetParas[i].textContent = '';
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField.focus();
+
+  lastResult.style.backgroundColor = 'white';
+
+  randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+};
+
+const autoGuesser = () => {
+  guessField.value = (maxNumber + minNumber) /2;
+  guessSubmit.click();
+
+  while (guessed == 'no'){
+    if(lowOrHi.textContent == "Last guess was too low!!"){
+      guessField.value = (maxNumber + [...guessInputs].pop()) / 2;
+      guessSubmit.click();
+      console.log('arvaus');
+    }else {
+      guessField.value = (minNumber + [...guessInputs].pop()) / 2;
+      guessSubmit.click();
+      console.log('arvaus');
+
+    }
   }
 };
 
-
-const sortPrice = (menu) => {
-  const sortedMenu = menu.sort((a, b) => {
-    return b.price - a.price;
-  });
-  console.log(sortedMenu);
-
-};
-
-const underFiveEuros = (menu) => {
-  const underFive = menu.filter(price => price.price < 5);
-  console.log(underFive);
-};
-
-
-const raisePrice = (menu) => {
-  const newPrice = menu.map(course => {
-    course.price = (course.price * 1.15).toFixed(2);
-    return course;
-  });
-  console.log(newPrice);
-
-};
-
-const sumMenu = (menu) => {
-  const sum = menu.reduce((total, current) => {
-    return total + parseFloat(current.price);
-  }, 0);
-  console.log('All items on menu together cost: ' + sum + '€');
-};
-
-
-//B
-
-const fazerVege = (Lunchmenu) => {
-  console.log('Vegaaniannokset:');
-    for (const days of Lunchmenu.LunchMenus) {
-      if(days.DayOfWeek != 'Maanantai'){
-        break;
-      }
-      for (const menus of days.SetMenus) {
-        for (const courses of menus.Meals) {
-          if (courses.Diets.includes('Veg')) {
-            console.log(courses.Name);
-          }
-        }
-      }
-    }
-};
-
-
-validateMeal(list);
-sortPrice(list);
-underFiveEuros(list);
-raisePrice(list);
-sumMenu(list);
-fazerVege(Lunchmenu);
-
-
-
-
-
-
-
-
-
-
+autoGuesser();
